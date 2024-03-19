101a1da529f2b139231d39bfc53d893914407faf
LegendWeaver <alexiaannsmith@gmail.com>
2024-03-19 10:33:10 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 
 int main() {
+    string start;
     cout <<"Welcome to Bippo's Quest! You are Bippo's new guardian, correct?\n\n"<< endl;
-    cin >> Start;
+    cin >> start;
+
+    if (start == yes || start == sure || start == YES ||){}
 }
